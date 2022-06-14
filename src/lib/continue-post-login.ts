52997c4491424f57dd3f10890e2bd7958c3f25a5
Felix Colaci <felix.colaci@okta.com>
2022-06-14 18:15:06 +0100

    fix: base64 decoding was broken in browser

--- src/lib/continue-post-login.ts
+++ src/lib/continue-post-login.ts
@@ -1,43 +1,46 @@
-import type { Auth0Session } from "./session.store";
+import { Base64 } from 'js-base64';
+import type { Auth0Session } from './session.store';
 
-const base_path = 'process.env.BASE_PATH'
+const base_path = 'process.env.BASE_PATH';
 
-export const continuePostLogin = async (token: string, sessionData: Auth0Session): Promise<{state: string, continueToken: string}> => {
-    const issuer = getIssuer(sessionData.sessionToken)
-    console.log(issuer)
-    try {
+export const continuePostLogin = async (
+	token: string,
+	sessionData: Auth0Session
+): Promise<{ state: string; continueToken: string }> => {
+	const issuer = getIssuer(sessionData.sessionToken);
+	console.log(issuer);
+	try {
+		var details = {
+			continueToken: token
+		};
 
-        var details = {
-            continueToken: token
-        }
-        
-        var formBody = [];
-        for (var property in details) {
-          var encodedKey = encodeURIComponent(property);
-          var encodedValue = encodeURIComponent(details[property]);
-          formBody.push(encodedKey + "=" + encodedValue);
-        }
+		var formBody = [];
+		for (var property in details) {
+			var encodedKey = encodeURIComponent(property);
+			var encodedValue = encodeURIComponent(details[property]);
+			formBody.push(encodedKey + '=' + encodedValue);
+		}
 
-        const rawResponse = await fetch(`https://${issuer}/continue?state=${sessionData.state}`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-            },
-            body: formBody.join("&")
-        })
-        console.log(rawResponse)
-        const response = await rawResponse.json();
-        console.log(response)
-        return response.token
-    } catch (error) {
-       console.error(error);
-       throw error
-    }
-}
+		const rawResponse = await fetch(`https://${issuer}/continue?state=${sessionData.state}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+			},
+			body: formBody.join('&')
+		});
+		console.log(rawResponse);
+		const response = await rawResponse.json();
+		console.log(response);
+		return response.token;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+};
 
 const getIssuer = (sessionToken) => {
-    const payload = sessionToken.split('.')[1];
-    const decodedPayload = atob(payload);
-    const { iss } = JSON.parse(decodedPayload);
-    return iss;
-}
+	const payload = sessionToken.split('.')[1];
+	const decodedPayload = Base64.decode(payload);
+	const { iss } = JSON.parse(decodedPayload);
+	return iss;
+};
--- src/lib/session.store.ts
+++ src/lib/session.store.ts
@@ -1,39 +1,40 @@
-import { writable } from "svelte/store";
-import { continuePostLogin } from "./continue-post-login";
-import { signData } from "./sign-data";
+import { writable } from 'svelte/store';
+import { continuePostLogin } from './continue-post-login';
+import { signData } from './sign-data';
+import { Base64 } from 'js-base64';
 
 export interface Auth0Session {
-    sessionToken?: string;
-    state?: string;
-    continueToken?: string;
-    action?: string
+	sessionToken?: string;
+	state?: string;
+	continueToken?: string;
+	action?: string;
 }
-
 
 const getIssuer = (sessionToken) => {
-    const payload = sessionToken.split('.')[1];
-    const decodedPayload = atob(payload);
-    const { iss } = JSON.parse(decodedPayload);
-    return iss;
-}
+	const payload = sessionToken.split('.')[1];
+	console.log(payload);
+	const decodedPayload = Base64.decode(payload);
+	const { iss } = JSON.parse(decodedPayload);
+	return iss;
+};
 
 function createSessionStore() {
-    const {subscribe, set, update} = writable({} as Auth0Session)
-    return {
-        subscribe,
-        reset: () => {},
-        init: (data: Auth0Session) => set(data),
-        dispatch: async (data: unknown) => {
-            let sess: Auth0Session;
-            subscribe($ => sess = $)();
-            const continueToken = await signData(data, sess);
-            const action = `https://${getIssuer(sess.sessionToken)}/continue?state=${sess.state}`;
-            update((sess) => {
-                return {...sess, continueToken, action} as Auth0Session
-                // await continuePostLogin(continueToken, sess)
-            })
-        }
-    }
+	const { subscribe, set, update } = writable({} as Auth0Session);
+	return {
+		subscribe,
+		reset: () => {},
+		init: (data: Auth0Session) => set(data),
+		dispatch: async (data: unknown) => {
+			let sess: Auth0Session;
+			subscribe(($) => (sess = $))();
+			const continueToken = await signData(data, sess);
+			const action = `https://${getIssuer(sess.sessionToken)}/continue?state=${sess.state}`;
+			update((sess) => {
+				return { ...sess, continueToken, action } as Auth0Session;
+				// await continuePostLogin(continueToken, sess)
+			});
+		}
+	};
 }
 
-export const auth0Session = createSessionStore()
+export const auth0Session = createSessionStore();
