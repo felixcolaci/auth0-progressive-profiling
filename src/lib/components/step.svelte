d92498fc73b37732b87208b9b12acce32ab27b00
Felix Colaci <felix.colaci@okta.com>
2022-08-24 10:55:35 +0200

    fix: remove default page name

--- src/lib/components/step.svelte
+++ src/lib/components/step.svelte
@@ -1,43 +1,40 @@
 <script lang="ts">
-  import { local } from "$lib/localStore";
-  import lodash from 'lodash'
-  const { camelCase } = lodash;
-  import { toTitleCase } from "$lib/title";
+	import { local } from '$lib/localStore';
+	import lodash from 'lodash';
+	const { camelCase } = lodash;
+	import { toTitleCase } from '$lib/title';
 
-  import type { JsonBool, JsonString } from "$lib/types/json";
+	import type { JsonBool, JsonString } from '$lib/types/json';
 
-  import { writable, type Writable } from "svelte/store";
+	import { writable, type Writable } from 'svelte/store';
 
+	export let name: string;
+	export let multi: Writable<JsonBool>;
+	const store = name !== undefined ? local<JsonString>(camelCase(name), {}) : writable({});
 
-  export let name: string;
-  export let multi: Writable<JsonBool>;
-  const store = name !== undefined ? local<JsonString>(camelCase(name), {}) : writable({});
+	multi.update((v) => {
+		v[name] = false;
 
-  multi.update(v => {
-    v[name] = false;
+		return v;
+	});
 
-    return v;
-  });
+	let visible = false;
 
-  let visible = false;
-
-  multi.subscribe(v => (visible = v[name]));
-  
+	multi.subscribe((v) => (visible = v[name]));
 </script>
 
 {#if visible}
-  <div class="step">
-    {#if name !== 'default'}
-      <h4>{toTitleCase(name)}</h4>
-      {:else}
-      <h4>Basic Information</h4>
-    {/if}
-    <slot {store} />
-  </div>
+	<div class="step">
+		{#if name !== 'default'}
+			<h4>{toTitleCase(name)}</h4>
+		{/if}
+		<slot {store} />
+	</div>
 {/if}
+
 <style lang="scss">
-  .step {
-    display: flex;
-    flex-direction: column;
-  }
+	.step {
+		display: flex;
+		flex-direction: column;
+	}
 </style>
